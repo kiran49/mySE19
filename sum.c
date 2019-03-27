372180403dfe21fed4b7b99c3c57008a3f748f99
kiran <sbejugam@my.okcu.edu>
2019-03-27 10:24:23 -0500

    #this is second version

--- sum.c
+++ sum.c
@@ -4,17 +4,20 @@
 main()
 {
   //intilisation of varaibles
-  int i,Num,Sum = 0;
+  int i,Num,Sum = 0,count;
+  float average;
   //taking numbers from user
   printf("please enter the value of the number:\n");
   scanf("%d",&Num);
   //display numbers
   printf("the value of number is :%d\n",Num);
   //calculations
-  for(i=1;i <=  Num; i++)
+  for(i=1;i <=  Num; i++){
     Sum +=i;
+  count = i;
+}
   //display output
   printf("the sum of numbers from 1 to Num = %d is Sum= %d\n",Num,Sum);
-
+  printf("the average of numbers is :%f\n",Sum/i);
   return 0;
 }// end of main
